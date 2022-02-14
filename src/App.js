79c6630c19b1ca048672a48527e5e03ee52cac80
ept-huzaifa <noorhuzaifa11@gmail.com>
2022-02-14 11:37:57 +0500

    code cleanup

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
+    <div className="App text-center">
       <h1 className="text-red-400 font-thin p-20">Hello World</h1>
     </div>
   );
